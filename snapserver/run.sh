758be7648936e5fc1a609b3ab017bee947200f66
phrenic <nick@nickhurley.co.uk>
2023-12-07 00:32:25 +0000

    Update run.sh

--- snapserver/run.sh
+++ snapserver/run.sh
@@ -34,8 +34,6 @@ echo "debug = $(bashio::config 'logging.enabled')" >> "${config}"
 
 echo "[server]" >> "${config}"
 echo "threads = $(bashio::config 'server.threads')" >> "${config}"
-
-echo "[server]" >> "${config}"
 echo "datadir = $(bashio::config 'server.datadir')" >> "${config}"
 
 bashio::log.info "Starting SnapServer..."
